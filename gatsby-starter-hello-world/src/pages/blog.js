603c958c9af442818c4efad18de409880aae03b8
camilo contreras <camilo.coo250@gmail.com>
2019-09-06 09:21:33 -0500

    blog page created

---
+++ gatsby-starter-hello-world/src/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class blog extends Component {
+    render() {
+        return (
+            <div>
+        this is out blog page
+                
+            </div>
+        )
+    }
+}
+
--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () =>
+<div>
+    <h1>This will be our homepage</h1>
+    <div>Hello world!</div>
+</div>
+ 
