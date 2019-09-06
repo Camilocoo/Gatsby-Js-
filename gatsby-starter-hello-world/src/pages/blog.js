0bd61831cfeee7c22e9e6ee2475c998e50dda3c6
camilo contreras <camilo.coo250@gmail.com>
2019-09-06 09:42:39 -0500

    done working with links

--- gatsby-starter-hello-world/src/pages/blog.js
+++ gatsby-starter-hello-world/src/pages/blog.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import {Link} from "gatsby";
+
 
 export default class blog extends Component {
     render() {
         return (
-            <div>
-        this is out blog page
-                
-            </div>
+            <React.Fragment>
+                <div>
+                    this is out blog page
+                </div><br/>
+                <Link to="/">Home page</Link>
+            </React.Fragment>
         )
     }
 }
--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import {Link} from "gatsby";
 
 export default () =>
 <div>
     <h1>This will be our homepage</h1>
     <div>Hello world!</div>
+    <Link to="/blog/">blog page using Link </Link><br/>
     <a href="/blog/">blog page</a><br/>
     <a href="https://www.gatsbyjs.org">gatsby webpage</a>
 
