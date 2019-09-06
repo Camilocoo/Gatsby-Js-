69721bf18129c80e160ff4865b6429cf38e305a2
camilo contreras <camilo.coo250@gmail.com>
2019-09-06 09:34:56 -0500

    two tyes of links

--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -4,5 +4,8 @@ export default () =>
 <div>
     <h1>This will be our homepage</h1>
     <div>Hello world!</div>
+    <a href="/blog/">blog page</a><br/>
+    <a href="https://www.gatsbyjs.org">gatsby webpage</a>
+
 </div>
  
