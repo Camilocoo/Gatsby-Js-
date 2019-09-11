12c81d8f11d70bb909e27545820feadfde3d090d
camilo contreras <camilo.coo250@gmail.com>
2019-09-11 18:52:41 -0500

    gatsby sharp and imgae added to tht gatsbyconfig

--- gatsby-starter-hello-world/gatsby-config.js
+++ gatsby-starter-hello-world/gatsby-config.js
@@ -22,6 +22,8 @@ siteMetadata:{
         path: `${__dirname}/src/images/`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`
 
