986b4eeb47531debff60f1af92bc0f05547f41e2
camilo contreras <camilo.coo250@gmail.com>
2019-09-12 13:31:13 -0500

    gatsby-image plugin working

--- gatsby-starter-hello-world/src/pages/images.js
+++ gatsby-starter-hello-world/src/pages/images.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Layout from "../components/layout"
+import React from 'react';
+import Layout from "../components/layout";
+import Images from "../examples/images";
 const images = () => {
     return (
         <div>
             <Layout>
                 Hello world from images
+
+                <br/>
+                <Images/>
             </Layout>
         </div>
     )
