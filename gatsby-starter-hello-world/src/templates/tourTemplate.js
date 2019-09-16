79c736ff41aa6a84409075fb14a368f044f311e2
camilo contreras <camilo.coo250@gmail.com>
2019-09-16 14:32:23 -0500

    create pages working

---
+++ gatsby-starter-hello-world/src/templates/tourTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TourTemplate = () => {
+    return (
+        <div>
+            hello from tour template
+        </div>
+    )
+}
+
+
+export default TourTemplate
